613ce06137ed1d60caa51320c82fce4af0b7dc15
yusif.hasanov17@gmail.com <yusif.hasanov17@gmail.com>
2022-10-15 18:38:17 +0400

    test

---
+++ DataAccess/Concrete/EntityFramework/EfContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfContext:DbContext
+    {
+       
+           protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=127.0.0.1192.168.30.20;uid=root;pwd=12345;database=test";
+            base.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+    }
+}
